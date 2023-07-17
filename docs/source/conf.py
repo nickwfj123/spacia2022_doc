40ef1fd0572793c197fe570a64388762254aad44
FJWU <54650699+nickwfj123@users.noreply.github.com>
2023-07-16 21:01:00 -0500

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Spacia'
+copyright = '2023, Tao Wang Lab'
+author = 'Tao Wang Lab'
 
 release = '0.1'
 version = '0.1.0'
