814a876d9ae045baed20328062f32a4801f84480
FJWU <54650699+nickwfj123@users.noreply.github.com>
2023-11-27 10:49:50 -0600

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -62,4 +62,4 @@ html_theme = "sphinx_rtd_theme"
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
 
-html_logo = "../assets/spaica_logo.png"
+html_logo = "../assets/spacia_logo.png"
